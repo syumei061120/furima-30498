758d40e3d51d18e557de83d8351a93b02ff4a39c
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-05 11:57:43 +0900

    レビュー#2のコメント内容修正（バリデーションの正規表現を変数化）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    common_name_validation = /\A[ぁ-んァ-ン一-龥]/
+
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :family_name, format: { with: common_name_validation, message: 'Full-width characters.' }
+    validates :first_name, format: { with: common_name_validation, message: 'Full-width characters.' }
+    validates :family_name_kana, format: { with: common_name_validation, message: 'Full-width katakana characters.' }
+    validates :first_name_kana, format: { with: common_name_validation, message: 'Full-width katakana characters.' }
     validates :birthday
   end
 
