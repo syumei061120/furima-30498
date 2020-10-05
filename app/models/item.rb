3a477912c3885b0cc14447d80745d09cf12f4904
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-05 21:34:54 +0900

    バリデーション 修正#1

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,20 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :explanation
-    validates :category_id
-    validates :status_id
-    validates :postage_user_id
-    validates :state_id
-    validates :shipping_date_id
+    validates :category_id, numericality: {other_than: 0, message: 'Select'}
+    validates :status_id, numericality: {other_than: 0, message: 'Select'}
+    validates :postage_user_id, numericality: {other_than: 0, message: 'Select'}
+    validates :state_id, numericality: {other_than: 0, message: 'Select'}
+    validates :shipping_date_id, numericality: {other_than: 0, message: 'Select'}
     validates :price, format: { with: /\A[0-9]+\z/ , message: 'Price Half-width number'}
+  end
+
+  with_options numericality: {other_than: 1} do
+  validates :category_id
+  validates :status_id
+  validates :postage_user_id
+  validates :state_id
+  validates :shipping_date_id
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
