c36614e0b5e0281fe00028f4b395d1e628732231
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-06 11:22:16 +0900

    レビュー#2のコメント修正（バリデーションをネスト化）

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,21 +6,15 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :explanation
-    validates :category_id
-    validates :status_id
-    validates :postage_user_id
-    validates :state_id
-    validates :shipping_date_id
+    with_options numericality: { other_than: 0, message: 'Select' } do
+      validates :category_id
+      validates :status_id
+      validates :postage_user_id
+      validates :state_id
+      validates :shipping_date_id
+    end
     validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }
   end
-
-  with_options numericality: { other_than: 0, message: 'Select' } do
-    validates :category_id
-    validates :status_id
-    validates :postage_user_id
-    validates :state_id
-    validates :shipping_date_id
-  end
-
+  
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
