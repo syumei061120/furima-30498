50653c710faa48202a000922abd3f13a7ff1a1a4
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-06 11:36:13 +0900

    レビュー#3のコメント修正　（priceのバリデーション修正）

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,8 +13,6 @@ class Item < ApplicationRecord
       validates :state_id
       validates :shipping_date_id
     end
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
   end
-  
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
