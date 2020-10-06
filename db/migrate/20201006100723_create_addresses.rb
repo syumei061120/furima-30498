f826976d772510dbc2cec12fb13b14a68b5afd52
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-06 19:31:20 +0900

    購入テーブル・住所テーブルをマイグレート

---
+++ db/migrate/20201006100723_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.references  :purchaces,     foreign_key: true
+      t.string      :postal_code,   null: false
+      t.integer     :state_id,      null: false
+      t.string      :city,          null: false
+      t.string      :address_line,  null: false
+      t.string      :building
+      t.string      :telephone,     null: false
+      t.timestamps
+    end
+  end
+end
