26e3ceb777d28fdb165400ec61f1a20ac1b33bd4
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-06 15:25:25 +0900

    商品出品画面の計算用jsに不具合があったので修正

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,20 +1,24 @@
 function check(){
-  function culTax(value){
-    value = value * 0.1;
-    return value;
-  }
-  function culProfit(value){
-    value = value - culTax(value)
-    return value;
-  }
-
   const price = document.getElementById("item-price")
-    value = price.value;
 
+  price.addEventListener('input',() => {
+    function culTax(value){
+      value = value * 0.1;
+      return value;
+    }
+    function culProfit(value){
+      value = value - culTax(value)
+      return value;
+    }
+
+    value = price.value;
+    console.log(value)
     let culAddTaxPriceForm = document.getElementById("add-tax-price");
     culAddTaxPriceForm.innerHTML = Math.floor(culTax(value));
     
+
     let profitForm = document.getElementById("profit");
     profitForm.innerHTML = Math.floor(culProfit(value));
+  })
 }
-setInterval(check, 1000);
+window.addEventListener("load", check);
