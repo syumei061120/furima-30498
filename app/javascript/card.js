7633ddab86990945ebc12423ea2929e9e73ef92f
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-07 18:12:33 +0900

    カード決済js修正#1

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -9,11 +9,12 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc:formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("card[number]"),
+      cvc:formData.get("card[cvc]"),
+      exp_month: formData.get("card[exp_month]"),
+      exp_year: `20${formData.get("card[exp_year]")}`,
     };
+    console.log();
     Payjp.createToken(card, (status, response)=>{
       if (status == 200){
         const token = response.id;
@@ -22,10 +23,10 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", token0bj);
       }
  
-      document.getElementById("order_number").removeAttribute("name");
-      document.getElementById("order_cvc").removeAttribute("name");
-      document.getElementById("order_exp_month").removeAttribute("name");
-      document.getElementById("order_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
