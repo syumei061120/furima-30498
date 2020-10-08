bda0c6b45a2ae954f30f7c0e464e4152658cec36
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-08 10:22:40 +0900

    コントローラー設定

--- app/controllers/purchaces_controller.rb
+++ app/controllers/purchaces_controller.rb
@@ -1,2 +1,41 @@
 class PurchacesController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  def index
+    @item = Item.find(9)
+    @purchace_address = PurchaceAddress.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+
+  end
+
+  def create
+    @purchace_address = PurchaceAddress.new(purchace_params)
+    binding.pry
+    @item = Item.find(9)
+
+    if @purchace_address.valid?
+      pay_item
+      @purchace_address.save
+      return redirect_to root_path
+    else
+      @item = Item.find(9)
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchace_params
+    params.permit(:item_id, :postal_code, :state_id, :city, :address_line, :building, :telephone, :token).merge(token: params[:token], user_id: current_user.id)
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchace_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
