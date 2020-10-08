d6b991f3c56d5e17a2192775d87b0c771bf847d6
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-08 11:41:05 +0900

    factorybotを作成

---
+++ spec/factories/purchace_address.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :purchace_address do
+    user_id  { Faker::Number.between(from: 1, to: 100) }
+    item_id { Faker::Number.between(from: 1, to: 1000) }
+    postal_code {"123-4567"}
+    state_id { Faker::Number.between(from: 1, to: 47) }
+    city { "大阪府" }
+    address_line { "大阪市" }
+    building { "通天閣" }
+    telephone { Faker::Number.between(from: 0_000_0000_000, to: 99_999_999_999)}
+    token {"tok_abcdefghijk00000000000000000"}
+  end
+end
