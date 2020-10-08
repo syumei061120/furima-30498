c2316bde87ace7bdbc3d116091de7e0a20d06a65
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-08 10:19:56 +0900

    カード決済js修正#2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,26 +1,26 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY)
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
 
-  form.addEventListener("submit", (e)=>{
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
-
+    console.log(formData.get("number"))
     const card = {
-      number: formData.get("card[number]"),
-      cvc:formData.get("card[cvc]"),
-      exp_month: formData.get("card[exp_month]"),
-      exp_year: `20${formData.get("card[exp_year]")}`,
+      number: formData.get("number"),
+      cvc:formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
     };
-    console.log();
-    Payjp.createToken(card, (status, response)=>{
-      if (status == 200){
+ 
+    Payjp.createToken(card, (status, response) => { 
+      if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const token0bj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", token0bj);
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
  
       document.getElementById("card-number").removeAttribute("name");
