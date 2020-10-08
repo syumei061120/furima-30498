dc17efdbedbdddfd0c97b7306c906cdb2f11e605
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-08 10:22:22 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :items, only: [:idex, :new, :show, :edit, :create] do
+    resources :purchaces
+  end
+
+  resources :addresses
 end
