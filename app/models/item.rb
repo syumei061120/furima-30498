315bfd603a81cb01d0e52d5328ef79d2de502678
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-09 11:38:32 +0900

    実装機能確認時の修正　バリデーションのメッセージ・商品情報編集後の画面遷移先修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,6 @@ class Item < ApplicationRecord
       validates :state_id
       validates :shipping_date_id
     end
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   end
 end
