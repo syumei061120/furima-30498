92d09e7f6147bf81a55731dd3d621a87f54296c2
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-12 23:59:49 +0900

    カード決済用js、金額計算用jsを修正　対象のURLを指定

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,22 +1,24 @@
-function check(){
-  const price = document.getElementById("item-price")
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
+  function check(){
+    const price = document.getElementById("item-price")
 
-  price.addEventListener('input', function() {
-    function culTax(value){
-      value = value * 0.1;
-      return value;
-    }
-    function culProfit(value){
-      value = value - Math.floor(culTax(value))
-      return value;
-    }
-    value = price.value;
+    price.addEventListener('input', function() {
+      function culTax(value){
+        value = value * 0.1;
+        return value;
+      }
+      function culProfit(value){
+        value = value - Math.floor(culTax(value))
+        return value;
+      }
+      value = price.value;
 
-    let culAddTaxPriceForm = document.getElementById("add-tax-price");
-    culAddTaxPriceForm.innerHTML = Math.floor(culTax(value));
+      let culAddTaxPriceForm = document.getElementById("add-tax-price");
+      culAddTaxPriceForm.innerHTML = Math.floor(culTax(value));
 
-    let profitForm = document.getElementById("profit");
-    profitForm.innerHTML = Math.floor(culProfit(value));
-  })
+      let profitForm = document.getElementById("profit");
+      profitForm.innerHTML = Math.floor(culProfit(value));
+    })
+  }
+  window.addEventListener('load', check)
 }
-window.addEventListener('load', check)
