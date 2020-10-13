d5f5553eed7470f12b53fc89878f383a98de9b3f
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-13 12:02:26 +0900

    console.log-delete

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,5 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
-  console.log("aaaa")
   const ImageList = document.getElementById('image-list');
 
     //画像を表示する関数
