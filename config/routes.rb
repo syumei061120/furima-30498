ea8fef7bb09aba54078686f666d86d5cce316d84
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-16 18:14:45 +0900

    deviseを再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers:{
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'user/registrations'
+  }
+  
   root to: 'items#index'
   resources :items do
     resources :purchaces, only:[:index, :create]
