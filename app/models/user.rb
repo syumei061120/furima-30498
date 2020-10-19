b905074d7092ddfb30527ef3b0a2ff5321aad729
syumei061120 <hideakitanaka1994@gmail.com>
2020-10-19 12:29:46 +0900

    userモデルで過去の認証情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,21 @@ class User < ApplicationRecord
   has_many :comments
   has_many :sns_credentials
 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
+  end
+
   name_validation = /\A[ぁ-んァ-ン一-龥]/
   name_kana_validation = /\A[ァ-ヶー－]+\z/
 
